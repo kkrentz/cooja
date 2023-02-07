0f619126c35c8c57acac87852a0f0b7e1224eb28
kkrentz <konrad.krentz@gmail.com>
2023-02-07 09:35:44 +0100

    Add frequency-selective RadioMedium

--- java/org/contikios/cooja/ExtensionManager.java
+++ java/org/contikios/cooja/ExtensionManager.java
@@ -55,6 +55,7 @@ import org.contikios.cooja.plugins.ScriptRunner;
 import org.contikios.cooja.plugins.TimeLine;
 import org.contikios.cooja.plugins.VariableWatcher;
 import org.contikios.cooja.plugins.Visualizer;
+import org.contikios.cooja.radiomediums.FrequencySelectiveUDGM;
 import org.contikios.cooja.radiomediums.DirectedGraphMedium;
 import org.contikios.cooja.radiomediums.LogisticLoss;
 import org.contikios.cooja.radiomediums.SilentRadioMedium;
@@ -92,6 +93,7 @@ public class ExtensionManager {
     registerBuiltinPlugin(MspStackWatcher.class);
     registerBuiltinPlugin(MspCycleWatcher.class);
 
+    registerBuiltinRadioMedium(FrequencySelectiveUDGM.class);
     registerBuiltinRadioMedium(UDGM.class);
     registerBuiltinRadioMedium(UDGMConstantLoss.class);
     registerBuiltinRadioMedium(DirectedGraphMedium.class);
